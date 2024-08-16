ae6499abdd3d81bcbaf7cde76359f6f418331db8
Carlos Tighe <carlos.tighe@insight-centre.org>
2024-08-16 16:25:40 +0100

    update user settings basemap

---
+++ src/User/UserSettingsDialog copy.jsx
@@ -0,0 +1,125 @@
+import MarxanDialog from "../MarxanDialog";
+import MenuItem from "@mui/material/MenuItem";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+/*
+ * Copyright (c) 2020 Andrew Cottam.
+ *
+ * This file is part of marxanweb/marxan-client
+ * (see https://github.com/marxanweb/marxan-client).
+ *
+ * License: European Union Public Licence V. 1.2, see https://opensource.org/licenses/EUPL-1.2
+ */
+import React from "react";
+import Select from "@mui/material/Select";
+
+class UserSettingsDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { saveEnabled: false };
+    this.options = {};
+  }
+  setOption(key, value) {
+    this.setState({ saveEnabled: true });
+    this.options[key] = value;
+    this.props.saveOptions(this.options);
+  }
+  updateOptions() {
+    this.props.onOk();
+  }
+  changeBasemap(event, key, payload) {
+    var basemap = this.props.basemaps[key];
+    this.props.changeBasemap(basemap);
+    this.setOption("BASEMAP", basemap.name);
+  }
+  toggleUseFeatureColors(evt, isInputChecked) {
+    this.setOption("USEFEATURECOLORS", isInputChecked);
+  }
+  toggleShowWelcomeScreen(evt, isInputChecked) {
+    this.setOption("SHOWWELCOMESCREEN", isInputChecked);
+  }
+  setReportUnit(event, value) {
+    this.setOption("REPORTUNITS", value);
+  }
+  render() {
+    return (
+      <MarxanDialog
+        {...this.props}
+        contentWidth={370}
+        offsetY={80}
+        showCancelButton={false}
+        onOk={this.updateOptions.bind(this)}
+        helpLink={"user.html#user-settings"}
+        title="Settings"
+      >
+        {
+          <div key="k14">
+            <Select
+              floatingLabelText={"Basemap style"}
+              floatingLabelFixed={true}
+              underlineShow={false}
+              menuItemStyle={{ fontSize: "12px" }}
+              labelStyle={{ fontSize: "12px" }}
+              style={{ width: "260px" }}
+              value={this.props.basemap}
+              onChange={this.changeBasemap.bind(this)}
+            >
+              {this.props.basemaps.map((item) => {
+                return (
+                  <MenuItem
+                    value={item.name}
+                    key={item.name}
+                    primaryText={item.alias}
+                    style={{ fontSize: "12px" }}
+                    title={item.description}
+                  />
+                );
+              })}
+            </Select>
+
+            <div style={{ paddingBottom: "10px" }}>
+              <div className={"userSetting"}>Area units</div>
+              <RadioGroup
+                name="reportUnitType"
+                defaultSelected={this.props.userData.REPORTUNITS}
+                onChange={this.setReportUnit.bind(this)}
+              >
+                <Radio
+                  value="m2"
+                  label="m2"
+                  className={"radioButton"}
+                  style={{ width: "60px", display: "inline-block" }}
+                  inputStyle={{ width: "40px" }}
+                  labelStyle={{ width: "40px" }}
+                  iconStyle={{ marginRight: "3px" }}
+                />
+                <Radio
+                  value="Ha"
+                  label="Ha"
+                  className={"radioButton"}
+                  style={{ width: "60px", display: "inline-block" }}
+                  inputStyle={{ width: "40px" }}
+                  labelStyle={{ width: "40px" }}
+                  iconStyle={{ marginRight: "3px" }}
+                />
+                <Radio
+                  value="Km2"
+                  label="Km2"
+                  className={"radioButton"}
+                  style={{ width: "60px", display: "inline-block" }}
+                  inputStyle={{ width: "40px" }}
+                  labelStyle={{ width: "40px" }}
+                  iconStyle={{ marginRight: "3px" }}
+                />
+              </RadioGroup>
+            </div>
+            {/*	<Checkbox label="Use feature colours" style={{fontSize:'12px'}} checked={this.props.userData.USEFEATURECOLORS} onCheck={this.toggleUseFeatureColors.bind(this)} />
+						<Checkbox label="Show welcome screen at startup" style={{fontSize:'12px'}} checked={this.props.userData.SHOWWELCOMESCREEN} onCheck={this.toggleShowWelcomeScreen.bind(this)} />*/}
+          </div>
+        }
+      </MarxanDialog>
+    );
+  }
+}
+
+export default UserSettingsDialog;
--- src/User/UserSettingsDialog.jsx
+++ src/User/UserSettingsDialog.jsx
@@ -1,125 +1,126 @@
+import React, { useState } from "react";
+
+import Checkbox from "@mui/material/Checkbox";
+import { Divider } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import FormLabel from "@mui/material/FormLabel";
+import InputLabel from "@mui/material/InputLabel";
 import MarxanDialog from "../MarxanDialog";
 import MenuItem from "@mui/material/MenuItem";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
-/*
- * Copyright (c) 2020 Andrew Cottam.
- *
- * This file is part of marxanweb/marxan-client
- * (see https://github.com/marxanweb/marxan-client).
- *
- * License: European Union Public Licence V. 1.2, see https://opensource.org/licenses/EUPL-1.2
- */
-import React from "react";
 import Select from "@mui/material/Select";
 
-class UserSettingsDialog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { saveEnabled: false };
-    this.options = {};
-  }
-  setOption(key, value) {
-    this.setState({ saveEnabled: true });
-    this.options[key] = value;
-    this.props.saveOptions(this.options);
-  }
-  updateOptions() {
-    this.props.onOk();
-  }
-  changeBasemap(event, key, payload) {
-    var basemap = this.props.basemaps[key];
-    this.props.changeBasemap(basemap);
-    this.setOption("BASEMAP", basemap.name);
-  }
-  toggleUseFeatureColors(evt, isInputChecked) {
-    this.setOption("USEFEATURECOLORS", isInputChecked);
-  }
-  toggleShowWelcomeScreen(evt, isInputChecked) {
-    this.setOption("SHOWWELCOMESCREEN", isInputChecked);
-  }
-  setReportUnit(event, value) {
-    this.setOption("REPORTUNITS", value);
-  }
-  render() {
-    return (
-      <MarxanDialog
-        {...this.props}
-        contentWidth={370}
-        offsetY={80}
-        showCancelButton={false}
-        onOk={this.updateOptions.bind(this)}
-        helpLink={"user.html#user-settings"}
-        title="Settings"
-      >
-        {
-          <div key="k14">
-            <Select
-              floatingLabelText={"Basemap style"}
-              floatingLabelFixed={true}
-              underlineShow={false}
-              menuItemStyle={{ fontSize: "12px" }}
-              labelStyle={{ fontSize: "12px" }}
-              style={{ width: "260px" }}
-              value={this.props.basemap}
-              onChange={this.changeBasemap.bind(this)}
-            >
-              {this.props.basemaps.map((item) => {
-                return (
-                  <MenuItem
-                    value={item.name}
-                    key={item.name}
-                    primaryText={item.alias}
-                    style={{ fontSize: "12px" }}
-                    title={item.description}
-                  />
-                );
-              })}
-            </Select>
+const UserSettingsDialog = (props) => {
+  const [saveEnabled, setSaveEnabled] = useState(false);
+  const [options, setOptions] = useState({});
 
-            <div style={{ paddingBottom: "10px" }}>
-              <div className={"userSetting"}>Area units</div>
-              <RadioGroup
-                name="reportUnitType"
-                defaultSelected={this.props.userData.REPORTUNITS}
-                onChange={this.setReportUnit.bind(this)}
-              >
-                <Radio
-                  value="m2"
-                  label="m2"
-                  className={"radioButton"}
-                  style={{ width: "60px", display: "inline-block" }}
-                  inputStyle={{ width: "40px" }}
-                  labelStyle={{ width: "40px" }}
-                  iconStyle={{ marginRight: "3px" }}
-                />
-                <Radio
-                  value="Ha"
-                  label="Ha"
-                  className={"radioButton"}
-                  style={{ width: "60px", display: "inline-block" }}
-                  inputStyle={{ width: "40px" }}
-                  labelStyle={{ width: "40px" }}
-                  iconStyle={{ marginRight: "3px" }}
-                />
-                <Radio
-                  value="Km2"
-                  label="Km2"
-                  className={"radioButton"}
-                  style={{ width: "60px", display: "inline-block" }}
-                  inputStyle={{ width: "40px" }}
-                  labelStyle={{ width: "40px" }}
-                  iconStyle={{ marginRight: "3px" }}
-                />
-              </RadioGroup>
-            </div>
-            {/*	<Checkbox label="Use feature colours" style={{fontSize:'12px'}} checked={this.props.userData.USEFEATURECOLORS} onCheck={this.toggleUseFeatureColors.bind(this)} />
-						<Checkbox label="Show welcome screen at startup" style={{fontSize:'12px'}} checked={this.props.userData.SHOWWELCOMESCREEN} onCheck={this.toggleShowWelcomeScreen.bind(this)} />*/}
-          </div>
-        }
-      </MarxanDialog>
+  const setOption = (key, value) => {
+    setSaveEnabled(true);
+    setOptions((prevOptions) => {
+      const newOptions = { ...prevOptions, [key]: value };
+      props.saveOptions(newOptions);
+      return newOptions;
+    });
+  };
+
+  const updateOptions = () => {
+    props.onOk();
+  };
+
+  const changeBasemap = (event) => {
+    const basemap = props.basemaps.find(
+      (item) => item.name === event.target.value
     );
-  }
-}
+    props.changeBasemap(basemap);
+    setOption("BASEMAP", basemap.name);
+  };
+
+  const toggleUseFeatureColors = (event) => {
+    setOption("USEFEATURECOLORS", event.target.checked);
+  };
+
+  const toggleShowWelcomeScreen = (event) => {
+    setOption("SHOWWELCOMESCREEN", event.target.checked);
+  };
+
+  const setReportUnit = (event) => {
+    setOption("REPORTUNITS", event.target.value);
+  };
+
+  return (
+    <MarxanDialog
+      {...props}
+      maxWidth="md"
+      showCancelButton={false}
+      onOk={updateOptions}
+      helpLink={"user.html#user-settings"}
+      title="Settings"
+    >
+      <div key="k14">
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="protected-areas-label">Base Map</InputLabel>
+          <Select
+            labelId="basemap-style-id"
+            id="basemap-style"
+            value={props.basemap}
+            label="Change Basemap"
+            fullWidth
+            onChange={changeBasemap}
+          >
+            {props.basemaps.map((item) => (
+              <MenuItem
+                key={item.name}
+                value={item.name}
+                style={{ fontSize: "12px" }}
+                title={item.description}
+              >
+                {item.alias}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Divider />
+        <FormControl>
+          <FormLabel id="reportUnitType-label">Area Units</FormLabel>
+          <RadioGroup
+            aria-labelledby="reportUnitType-label"
+            value={props.userData.REPORTUNITS}
+            name="reportUnitType"
+            onChange={setReportUnit}
+          >
+            <FormControlLabel value="m2" control={<Radio />} label="m2" />
+            <FormControlLabel value="Ha" control={<Radio />} label="Ha" />
+            <FormControlLabel value="Km2" control={<Radio />} label="Km2" />
+          </RadioGroup>
+        </FormControl>
+        <Divider />
+
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={props.userData.USEFEATURECOLORS}
+                onChange={toggleUseFeatureColors}
+              />
+            }
+            label="Use Feature Colours"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={props.userData.SHOWWELCOMESCREEN}
+                onChange={toggleShowWelcomeScreen}
+              />
+            }
+            label="Show welcome screen at startup"
+          />
+        </FormGroup>
+      </div>
+    </MarxanDialog>
+  );
+};
 
 export default UserSettingsDialog;
