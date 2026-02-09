df05d1ab3b24565312d29e024758a3d71c9035a1
Carlos Tighe <carlos.tighe@insight-centre.org>
2026-02-09 19:05:12 +0000

    test

--- src/LogItem.jsx
+++ src/LogItem.jsx
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) 2020 Andrew Cottam.
- *
- * This file is part of marxanweb/marxan-client
- * (see https://github.com/marxanweb/marxan-client).
- *
- * License: European Union Public Licence V. 1.2, see https://opensource.org/licenses/EUPL-1.2
- */
 import * as React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
